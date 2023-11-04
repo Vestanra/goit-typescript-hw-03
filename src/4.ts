33d9e4c381a867cc67b29721e1a11fa2852e016c
Vestanra <35749970+Vestanra@users.noreply.github.com>
2023-11-04 18:28:49 +0200

    hw3

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,63 @@
+class Key {
+  private signature: number
+    constructor() {
+        this.signature = Math.round(Math.random()*1 + 1)
+    }
+    getSignature(): number {
+        return this.signature
+    }
+}
+
+class Person {
+    private name: string;
+    private key: Key;
+    constructor(key: Key, name: string) {
+        this.name = name;
+        this.key = new Key()
+    }
+
+    getKey() {
+        return this.key
+    }
+}
+
+abstract class House {
+    door: boolean;
+    key: Key;
+    tenants: Person[] = [];
+    constructor(key: Key) {
+      this.door = false;
+      this.key = key;
+  }
+
+    comeIn(person: Person) {
+        if (this.door) {
+            this.tenants.push(person)
+        }
+  }
+  
+  abstract openDoor(key: Key):void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key){
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true
+      console.log('The door is open')
+    } else {
+      this.door = false
+      console.log('The door is closed')
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
-const person = new Person(key);
+const person = new Person(key, "unknown");
 
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
